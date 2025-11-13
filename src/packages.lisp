f5501653e4f6ce770ad75540c510e819a594e0e1
Daniel Nussenbaum <daninus14@gmail.com>
2025-11-13 13:12:50 -0500

    not sure why this is autoindenting, but the main change was to delete one empty line and export the symbol `#:get-slots-to-encode`

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,86 +1,88 @@
+(cl:in-package #:common-lisp-user)
+
 (defpackage ejson
-  (:use #:cl)
-  (:export
+            (:use #:cl)
+            (:export
    ;;; Settings
-   #:*serialize-lisp-case-to-camel-case*
-   #:*deserialize-camel-case-to-lisp-case*
-   #:*deserialize-lisp-case-upcase*
-   
+             #:*serialize-lisp-case-to-camel-case*
+             #:*deserialize-camel-case-to-lisp-case*
+             #:*deserialize-lisp-case-upcase*
+             
    ;;; Read
-   #:parse   
+             #:parse   
 
    ;;;; Streaming reader
-   #:parser
-   #:make-parser
-   #:parse-next
-   #:parse-next-element
-   #:close-parser
-   #:with-parser
+             #:parser
+             #:make-parser
+             #:parse-next
+             #:parse-next-element
+             #:close-parser
+             #:with-parser
 
-   ;; Reading utils
-   #:span
+             ;; Reading utils
+             #:span
 
    ;;; Write
-   #:stringify
+             #:stringify
 
    ;;; Types
-   #:json-atom
-   #:json-element
+             #:json-atom
+             #:json-element
 
    ;;; Conditions
-   #:json-error
-   #:json-limit-error
-   #:json-parse-error
-   #:json-parse-limit-error
-   #:json-eof-error
-   #:json-write-error
-   #:json-write-limit-error
-   #:json-recursive-write-error
+             #:json-error
+             #:json-limit-error
+             #:json-parse-error
+             #:json-parse-limit-error
+             #:json-eof-error
+             #:json-write-error
+             #:json-write-limit-error
+             #:json-recursive-write-error
 
    ;;; Simple extensible writing
-   #:coerced-fields
-   #:coerce-key
+             #:coerced-fields
+             #:coerce-key
 
    ;;; Streaming Writer
-   #:writer
-   #:make-writer
-   #:close-writer
-   #:with-writer
+             #:writer
+             #:make-writer
+             #:close-writer
+             #:with-writer
 
-   ;; Extensible serialization
-   #:write-value
+             ;; Extensible serialization
+             #:write-value             
+             #:begin-array
+             #:write-values
+             #:end-array
+             #:with-array
+             #:write-array
+             #:get-slots-to-encode
 
-   #:begin-array
-   #:write-values
-   #:end-array
-   #:with-array
-   #:write-array
+             ;; Writer operations
+             #:begin-object
+             #:write-key
+             #:write-properties
+             #:end-object
+             #:with-object
+             #:write-object
 
-   ;; Writer operations
-   #:begin-object
-   #:write-key
-   #:write-properties
-   #:end-object
-   #:with-object
-   #:write-object
-
-   ;; Dynavar interface to the writer
-   #:*writer*
-   #:with-writer*
-   #:write-value*
-   #:begin-array*
-   #:write-values*
-   #:end-array*
-   #:with-array*
-   #:write-array*
-   #:begin-object*
-   #:write-key*
-   #:write-property*
-   #:write-properties*
-   #:end-object*
-   #:with-object*
-   #:write-object*)
-  (:import-from #:closer-mop)
-  (:import-from #:flexi-streams)
-  (:import-from #:float-features)
-  (:import-from #:uiop))
+             ;; Dynavar interface to the writer
+             #:*writer*
+             #:with-writer*
+             #:write-value*
+             #:begin-array*
+             #:write-values*
+             #:end-array*
+             #:with-array*
+             #:write-array*
+             #:begin-object*
+             #:write-key*
+             #:write-property*
+             #:write-properties*
+             #:end-object*
+             #:with-object*
+             #:write-object*)
+            (:import-from #:closer-mop)
+            (:import-from #:flexi-streams)
+            (:import-from #:float-features)
+            (:import-from #:uiop))
